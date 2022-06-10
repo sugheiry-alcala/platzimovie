71f5d200ba84a53bc6339316ed6ccb3eca88c835
Sugheiry Alcala <sugheiry@gmail.com>
2022-06-10 17:34:52 -0500

    configuración del entrono de desarrollo

---
+++ src/main.js
@@ -0,0 +1,4 @@
+
+
+console.log('Hello, mundo')
+console.log('API URL = https://DKCSDVLKSDM?api_jei=' + API_KEY)
---
+++ src/secrets.example.js
@@ -0,0 +1,2 @@
+// Crea por favor un archivo secrets.js con tu API_KEY
+const PAI_KEY = ''; 
